54b74525bd57b2ed0dd65b5a061e109b4f50ec7e
pepeminor <thang.nq.666@gmail.com>
2025-07-18 23:19:49 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,9 +6,37 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [deferredPrompt, setDeferredPrompt] = useState<Event | null>(null)
+  const [showInstallPrompt, setShowInstallPrompt] = useState(false)
+
+  useEffect(() => {
+    const handler = (e: any) => {
+      e.preventDefault() // chặn trình duyệt tự hiển thị
+      setDeferredPrompt(e)
+      setShowInstallPrompt(true) // bạn có thể hiện nút "Cài app" ở đây
+    }
+
+    window.addEventListener('beforeinstallprompt', handler)
+    return () => window.removeEventListener('beforeinstallprompt', handler)
+  }, [])
+
+  const handleInstallClick = async () => {
+    if (!deferredPrompt) return
+    ;(deferredPrompt as any).prompt()
+    const { outcome } = await (deferredPrompt as any).userChoice
+    console.log('User response to install prompt:', outcome)
+    setDeferredPrompt(null)
+    setShowInstallPrompt(false)
+  }
+
+
   return (
     <>
       <div>
+      <h1>LuckyGift</h1>
+      {showInstallPrompt && (
+        <button onClick={handleInstallClick}>🎁 Cài LuckyGift vào màn hình</button>
+      )}
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
