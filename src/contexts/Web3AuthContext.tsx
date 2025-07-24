8c8c5cf4542c910b52f33f9db68b376862b7797c
pepeminor <thang.nq.666@gmail.com>
2025-07-24 20:43:53 +0700

    run build

--- src/contexts/Web3AuthContext.tsx
+++ src/contexts/Web3AuthContext.tsx
@@ -2,12 +2,12 @@ import { WEB3AUTH_NETWORK } from "@web3auth/modal";
 import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 
 const clientId =
-  "BBF90jYyWvLAeRMq-RhykdJsIjqBBP7g1424n55o-BdLgeUN1S2HYO2BC6O-371YjuiW4iR-pWysn9KnCV0v6EA"; //SAPPHIRE_MAINNET
+  "BJ1-HANEzatKPG572CEES2nVJn9jXiNu8wqurhRiaALmD2QlPOqU__xtOBuTOrqQydN3ZBbTRiiXeHepb-A6lVw";
 
 const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
     clientId,
-    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
   },
 };
 
